b6acc66816655ef839de3aa2be9c0e34045f4eea
Dope Beats <reweeden@alaska.edu>
2017-11-09 22:51:27 -0900

    About halfway through the rails tutorial

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,24 @@
+class TeamsController < ApplicationController
+  def index
+    @teams = Team.all
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    @team.save
+    redirect_to @team
+  end
+
+  private
+  def team_params
+    params.require(:team).permit(:name)
+  end
+end
