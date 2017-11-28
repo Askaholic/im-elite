36f3135e7f18e3a05e37fb0a0d6ec0f830293b77
Dope Beats <reweeden@alaska.edu>
2017-11-27 19:28:51 -0900

    Added seeds for generating fake data (using Faker)

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,31 @@ seed_table(LeagueType, [
   "Comp",
   "Casual"
 ])
+
+
+# Insert DB data (at least 500 rows)
+row_count = 0
+
+for i in 1..5 do
+  org = Organization.create(name: Faker::University.name)
+  row_count += 1
+  teams = []
+  for i in 1..2 do
+    season = org.seasons.create(start_date: Faker::Date.between(3.months.ago, Date.today), end_date: Faker::Date.between(Date.today, 3.months.from_now))
+    row_count += 1
+    for i in 1..3 do
+      league = season.leagues.create(sport: Sport.find(rand(1..5)).name, l_type: LeagueType.find(rand(1..2)).name)
+      row_count += 1
+      for i in 1..7 do
+        teams << league.teams.create(name: Faker::Team.name)
+        row_count += 1
+      end
+    end
+  end
+  for i in 1..50 do
+    org.players.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, team_id: teams[rand(1..(teams.size - 1))].id)
+    row_count += 1
+  end
+end
+
+puts "Created " + row_count.to_s + " rows"
